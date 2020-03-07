a2e1b3216f917e4725fcbe354696e5cc9c755f88
Sasha K <alexander.kovaleff@gmail.com>
2020-03-07 14:25:16 +0100

    update stream/status url

--- ui.js
+++ ui.js
@@ -1,5 +1,5 @@
-const streamUrl = 'http://rosetta.whatbox.ca:16896/sasha-today-320.mp3';
-const statusUrl = 'http://rosetta.whatbox.ca:16896/status-json.xsl';
+const streamUrl = 'http://saturn.whatbox.ca:16896/sasha-today-320.mp3';
+const statusUrl = 'http://saturn.whatbox.ca:16896/status-json.xsl';
 
 const player = createAudioPlayer(
   window.chrome ? createMediaSource(streamUrl) : streamUrl,
