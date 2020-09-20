3662bf26b7393f84c7b280b693c3d51d497c9baf
Sasha Kovalyov <alexander.kovaleff@gmail.com>
2020-09-20 19:48:38 +0200

    fallback to ReadableStream if WrtiableStream isn’t supported

--- fetch-stream.js
+++ fetch-stream.js
@@ -3,11 +3,22 @@ function fetchStream(streamUrl, processChunk) {
 
   fetch(streamUrl, { signal: controller.signal })
     .then(({ body }) => {
-      return body.pipeTo(new WritableStream({ write: processChunk }))
+      if (typeof WritableStream === 'function') {
+        return body.pipeTo(new WritableStream({ write: processChunk }))
+      } else {
+        const reader = body.getReader();
+
+        reader.read().then(function process({done, value}) {
+          if (!done && value) {
+            processChunk(value);
+            return reader.read().then(process);
+          }
+        });
+      }
     })
     .catch((e) => console.log('fetching was stopped', e.toString()));
 
-  return () => {
+  return function cancelFetch() {
     if (!controller.signal.aborted) {
       controller.abort();
     }
